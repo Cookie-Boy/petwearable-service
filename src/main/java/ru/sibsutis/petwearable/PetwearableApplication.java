808d26e0b2f47d221f6fe7268e56c673f4e2510a
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-03-03 19:56:16 +0700

    Initial commit

---
+++ src/main/java/ru/sibsutis/petwearable/PetwearableApplication.java
@@ -0,0 +1,13 @@
+package ru.sibsutis.petwearable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetwearableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetwearableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sibsutis/petwearable/PetwearableApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sibsutis.petwearable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetwearableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
