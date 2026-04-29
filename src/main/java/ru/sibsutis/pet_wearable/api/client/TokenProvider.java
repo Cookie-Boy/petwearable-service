4d1c9eaa2774dc60052db67484b7aa5866a1c2e9
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-04-30 04:28:06 +0700

    Update TokenProvider

--- src/main/java/ru/sibsutis/pet_wearable/api/client/TokenProvider.java
+++ src/main/java/ru/sibsutis/pet_wearable/api/client/TokenProvider.java
@@ -1,24 +1,47 @@
 package ru.sibsutis.pet_wearable.api.client;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 @Component
 @RequiredArgsConstructor
 public class TokenProvider {
-    private final OAuth2AuthorizedClientManager clientManager;
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
     private final String clientRegistrationId = "pet-wearable";
+    private OAuth2AuthorizedClient authorizedClient;
+
+    @PostConstruct
+    public void init() {
+        refreshClientCredentials();
+    }
 
     public String getFreshToken() {
-        OAuth2AuthorizedClient client = clientManager.authorize(
-                OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
-                        .principal("service-account")
-                        .build()
-        );
+        refreshIfNeeded();
+        return authorizedClient.getAccessToken().getTokenValue();
+    }
 
-        return client != null ? client.getAccessToken().getTokenValue() : "token-is-null";
+    private void refreshClientCredentials() {
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId(clientRegistrationId)
+                .principal("service-account")
+                .build();
+
+        authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+        if (authorizedClient == null) {
+            throw new IllegalStateException("Failed to obtain token");
+        }
+    }
+
+    private void refreshIfNeeded() {
+        if (authorizedClient == null ||
+                authorizedClient.getAccessToken().getExpiresAt().isBefore(Instant.now().plusSeconds(60))) {
+            refreshClientCredentials();
+        }
     }
 }
