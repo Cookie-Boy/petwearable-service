b1b814a94542e392af8d8cd4b052055a803794f4
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-04-30 04:34:22 +0700

    Add OAuth2Config

---
+++ src/main/java/ru/sibsutis/pet_wearable/configuration/OAuth2ClientConfig.java
@@ -0,0 +1,35 @@
+package ru.sibsutis.pet_wearable.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+
+import java.util.Collections;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        // Провайдер только для client_credentials (не требует запроса)
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        // Отключаем попытки достать HttpServletRequest – передаём пустые атрибуты
+        authorizedClientManager.setContextAttributesMapper(contextAttributes -> Collections.emptyMap());
+        return authorizedClientManager;
+    }
+}
